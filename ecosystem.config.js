63a6630f50edcfbfdf61dfa63f798eb74b6917b8
Devesh Meena <nothefakedevesh@gmail.com>
2025-09-18 23:19:21 +0530

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,30 @@
-module.exports = {
+// ecosystem.config.js (ESM)
+export default {
     apps: [
       {
-        name: `frontend`,
+        name: "frontend",
         script: "npx",
         args: "vite preview",
-        env: {
-          PORT: 5173,
-          NODE_ENV: 'production',
-        },
+        env: { PORT: 5173, NODE_ENV: "production" },
       },
       {
-        name: `backend`,
+        name: "backend",
         script: "node",
         args: "server/dist/server.js",
         env: {
           PORT: 3001,
-          NODE_ENV: 'production',
+          NODE_ENV: "production",
         },
         watch: false,
         autorestart: true,
-        max_memory_restart: '1G',
+        max_memory_restart: "1G",
         instances: 1,
-        exec_mode: 'cluster',
+        exec_mode: "cluster",
         env_production: {
           PORT: 3001,
-          NODE_ENV: 'production',
+          NODE_ENV: "production",
         },
-      }
+      },
     ],
   };
   
